81875dfdd70231ff2eb510064828516d324ecbeb
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-15 23:08:02 -0700

    i lowk forgot

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
 
 namespace Redbox_Mobile_Command_Center_Server {
     class Program {
 
         static void Main(string[] args) {
-
+            
         }
+
+        
     }
 }
