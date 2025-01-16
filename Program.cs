ead9a08e13bdab8d2d21b3cea10ab876902a21e8
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-15 22:52:45 -0700

    quic didnt wanna work :(

--- Program.cs
+++ Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using QuicNet;
-using QuicNet.Streams;
-using QuicNet.Connections;
 
 namespace Redbox_Mobile_Command_Center_Server {
     class Program {
