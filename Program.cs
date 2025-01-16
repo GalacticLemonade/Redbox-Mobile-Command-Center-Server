6f496e546defba610017826a0271adbeee5298f5
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-15 22:51:23 -0700

    ok then?

--- Program.cs
+++ Program.cs
@@ -8,28 +8,10 @@ using QuicNet.Streams;
 using QuicNet.Connections;
 
 namespace Redbox_Mobile_Command_Center_Server {
-    internal class Program {
-        // fired when a client is connected
-        static void ClientConnected(QuicConnection connection) {
-            connection. += StreamOpened;
-        }
-
-        // fired when a new stream has been opened (It does not carry data with it)
-        static void StreamOpened(QuicStream stream) {
-            stream.On += StreamDataReceived;
-        }
-
-        // fired when a stream received full batch of data
-        static void StreamDataReceived(QuicStream stream, byte[] data) {
-            string decoded = Encoding.UTF8.GetString(data);
-
-            // Send back data to the client on the same stream
-            stream.Send(Encoding.UTF8.GetBytes("Ping back from server."));
-        }
+    class Program {
 
         static void Main(string[] args) {
-            QuicListener listener = new QuicListener(11000);
-            listener.OnClientConnected += ClientConnected;
+
         }
     }
 }
