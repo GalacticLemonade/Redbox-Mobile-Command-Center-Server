6fb704b2997707dfa30f54e9bd7b8fff43eaa0c9
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-16 18:07:36 -0700

    smith

--- Program.cs
+++ Program.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.Remoting.Messaging;
 
 namespace Redbox_Mobile_Command_Center_Server {
     class Program {
-
         static void Main(string[] args) {
-            
+            Console.WriteLine("Hi!");
+            Console.ReadLine();
         }
-
-        
     }
 }
