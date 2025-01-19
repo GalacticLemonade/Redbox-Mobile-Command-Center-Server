f19c29e4b8b09ecb1e7170d3ff03243bb6b70817
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-19 16:19:04 -0700

    Kiosk specific commands

--- Commands/ExecuteOnKioskCommand .cs
+++ Commands/ExecuteOnKioskCommand .cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Redbox_Mobile_Command_Center_Server.KioskCommands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +7,25 @@ using System.Threading.Tasks;
 
 namespace Redbox_Mobile_Command_Center_Server.Commands {
     public class ExecuteOnKioskCommand : ICommand {
+        // Internal registry for kiosk-specific commands
+        private static readonly Dictionary<string, ICommand> KioskCommands = new Dictionary<string, ICommand> {
+            { "test-comm", new TestCommCommand() }
+        };
+
         public async Task<string> Run(string[] arguments) {
             if (arguments.Length == 0) {
-                return "402\r\nError: No command specified for kiosk execution.";
+                return "Error: No command specified for kiosk execution.";
             }
 
             string innerCommandName = arguments[0];
             string[] innerArguments = arguments.Skip(1).ToArray();
 
-            if (CommandRegistry.Commands.TryGetValue(innerCommandName, out ICommand innerCommand)) {
-                Console.WriteLine($"Executing command '{innerCommandName}' on kiosk with arguments: {string.Join(" ", innerArguments)}");
+            if (KioskCommands.TryGetValue(innerCommandName, out ICommand innerCommand)) {
+                Console.WriteLine($"Executing kiosk command '{innerCommandName}' with arguments: {string.Join(" ", innerArguments)}");
                 return await innerCommand.Run(innerArguments);
             }
             else {
-                return $"400\r\nError: Unknown command '{innerCommandName}' for kiosk execution.";
+                return $"Error: Unknown kiosk command '{innerCommandName}'.";
             }
         }
     }
