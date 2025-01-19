ae8d44da54ca0481868901f4d77e4abbee045b7c
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-19 16:09:21 -0700

    Add new command

---
+++ Commands/ExecuteOnKioskCommand .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redbox_Mobile_Command_Center_Server.Commands {
+    public class ExecuteOnKioskCommand : ICommand {
+        public async Task<string> Run(string[] arguments) {
+            if (arguments.Length == 0) {
+                return "402\r\nError: No command specified for kiosk execution.";
+            }
+
+            string innerCommandName = arguments[0];
+            string[] innerArguments = arguments.Skip(1).ToArray();
+
+            if (CommandRegistry.Commands.TryGetValue(innerCommandName, out ICommand innerCommand)) {
+                Console.WriteLine($"Executing command '{innerCommandName}' on kiosk with arguments: {string.Join(" ", innerArguments)}");
+                return await innerCommand.Run(innerArguments);
+            }
+            else {
+                return $"400\r\nError: Unknown command '{innerCommandName}' for kiosk execution.";
+            }
+        }
+    }
+}
