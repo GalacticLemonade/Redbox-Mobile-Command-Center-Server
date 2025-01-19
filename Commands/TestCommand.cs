0f22d359fa6eeff0be1676da518af9ef02af7b0d
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-19 15:05:59 -0700

    Create TestCommand

---
+++ Commands/TestCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redbox_Mobile_Command_Center_Server.Commands {
+    public class TestCommand : ICommand {
+        public async Task<string> Run(string[] arguments) {
+            Console.WriteLine("Executing TestCommand");
+            return await Task.FromResult("TestCommand executed.");
+        }
+    }
+}
