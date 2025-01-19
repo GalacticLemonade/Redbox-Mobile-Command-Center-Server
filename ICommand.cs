4899f3860467ed214bf23b5a84cb78112f2f853c
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-19 14:52:41 -0700

    Implement ICommand

---
+++ ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redbox_Mobile_Command_Center_Server {
+    public interface ICommand {
+        Task<string> Run(string[] arguments);
+    }
+}
