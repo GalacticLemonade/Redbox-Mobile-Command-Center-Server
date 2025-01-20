890624d9ac0de763573889e9c639d0b15f36cf4d
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-19 18:21:11 -0700

    Add GetKioskIDCommand command

---
+++ KioskCommands/GetKioskIDCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redbox_Mobile_Command_Center_Server.KioskCommands {
+    public class GetKioskIDCommand : ICommand {
+        public async Task<string> Run(string[] arguments) {
+            string halResponse = await HALConnection.SendHALCommandAsync("SERVICE get-kiosk-id");
+
+            return await Task.FromResult(halResponse);
+        }
+    }
+}
